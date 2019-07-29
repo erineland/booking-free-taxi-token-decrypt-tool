2e4f29fa1d2cef3049824310b1da3e3547733d12
James Starkie <jamesstarkie@rideways.com>
2019-07-29 16:19:01 +0100

    Add timeGenerated and generatedBy fields

--- src/v2/encrypt.js
+++ src/v2/encrypt.js
@@ -1,5 +1,6 @@
-const argv = require('yargs').argv;
 const readline = require('readline');
+const argv = require('yargs').argv;
+const moment = require('moment');
 const Config = require('@rides/node-config').default;
 const { BkngToken } = require('@bookingcom/bkng-crypto');
 
@@ -17,14 +18,18 @@ const cli = readline.createInterface({
     output: process.stdout
 });
 
-cli.question('Enter the payload to encrypt: ', async payload => {
+cli.question('Enter the payload to encrypt: ', async input => {
     console.log();
 
     try {
+        const payload = JSON.parse(input);
+        payload.timeGenerated = moment.utc().format();
+        payload.generatedBy = 'BookingGo';
+
         const passphrase = argv.passphrase || await config.valueFor('booking.freeTaxiTokenPassphrase');
         const iv = argv.iv || await config.valueFor('booking.freeTaxiTokenIV');
         const bkngToken = new BkngToken(passphrase, iv);
-        const encryptedToken = await bkngToken.encrypt(payload);
+        const encryptedToken = await bkngToken.encrypt(JSON.stringify(payload));
 
         console.log('###################### ENCRYPTED TOKEN #######################');
         console.log(encryptedToken);
