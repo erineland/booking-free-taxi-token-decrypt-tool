f3c621f062e3c978b7e2434e8ab9b64168181d9d
James Starkie <jamesstarkie@rideways.com>
2019-07-30 10:45:16 +0100

    Output redeem url when generating a token

--- src/v2/encrypt.js
+++ src/v2/encrypt.js
@@ -6,6 +6,12 @@ const moment = require('moment');
 const Config = require('@rides/node-config').default;
 const { BkngToken } = require('@bookingcom/bkng-crypto');
 
+const urls = {
+    dev: 'https://taxi.dev.someonedrive.me',
+    qa: 'https://taxi.qa.someonedrive.me',
+    prod: 'https://taxi.booking.com',
+};
+
 const config = new Config({
     region: argv.region || 'eu-west-1',
     env: argv.env || 'qa',
@@ -31,15 +37,19 @@ const encrypt = async input => {
 
     console.log('\n######################### TOKEN BODY #########################');
     console.log(payload);
-    console.log('##############################################################\n');
+    console.log('##############################################################');
 
     const passphrase = argv.passphrase || await config.valueFor('booking.freeTaxiTokenPassphrase');
     const iv = argv.iv || await config.valueFor('booking.freeTaxiTokenIV');
     const bkngToken = new BkngToken(passphrase, iv);
     const encryptedToken = await bkngToken.encrypt(JSON.stringify(payload));
 
-    console.log('###################### ENCRYPTED TOKEN #######################');
+    console.log('\n###################### ENCRYPTED TOKEN #######################');
     console.log(encryptedToken);
+    console.log('##############################################################');
+
+    console.log('\n######################## REDEEM URL ##########################');
+    console.log(`${urls[config.options.env]}/promotions/free-taxi/${encryptedToken}`);
     console.log('##############################################################');
 }
 
