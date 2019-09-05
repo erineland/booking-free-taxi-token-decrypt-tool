75c48e33f63067c2502639d77705c3d296b21f6e
Hernan Bessera <hernanman_1@hotmail.com>
2019-09-05 11:43:24 +0100

    adds test

--- src/v2/decrypt.js
+++ src/v2/decrypt.js
@@ -4,44 +4,59 @@ const chalk = require('chalk');
 const Config = require('@rides/node-config').default;
 const { BkngToken } = require('@bookingcom/bkng-crypto');
 
-const config = new Config({
-    region: argv.region || 'eu-west-1',
-    env: argv.env || 'qa',
-    platform: 'web',
-    appName: 'grandcentral',
-    enableSSM: true,
-    localConfig: {},
-});
-
 const cli = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
 cli.question('Enter the token to decrypt: ', async token => {
-    console.log();
+    const config = new Config({
+        region: argv.region || 'eu-west-1',
+        env: argv.env || 'qa',
+        platform: 'web',
+        appName: 'grandcentral',
+        enableSSM: true,
+        localConfig: {},
+    });
 
-    let passphrase;
-    let iv;
+    let decryptedToken;
+
     try {
-
-        if (argv.phone) {
-            passphrase = argv.passphrase || await config.valueFor('booking.freeTaxiCellphonePassphrase');
-            iv = argv.iv || await config.valueFor('booking.freeTaxiCellphoneIV');
-        } else {
-            passphrase = argv.passphrase || await config.valueFor('booking.freeTaxiTokenPassphrase');
-            iv = argv.iv || await config.valueFor('booking.freeTaxiTokenIV');
-        }
-
-        const bkngToken = new BkngToken(passphrase, iv);
-        const decryptedToken = await bkngToken.decrypt(token);
-        
-        console.log(chalk.blue('######################### TOKEN BODY #########################'));
-        console.log(decryptedToken);
-        console.log(chalk.blue('##############################################################'));
+        const cryptoValues = await getValuesFromConfig(config, argv.phone);
+        decryptedToken = await decryptToken(token, cryptoValues);
     } catch (err) {
         console.error(chalk.red(err));
     }
 
+    if (decryptedToken) {
+        console.log(chalk.blue('######################### TOKEN BODY #########################'));
+        console.log(decryptedToken);
+        console.log(chalk.blue('##############################################################'));
+    }
+
     cli.close();
 });
+
+const decryptToken = async (token, cryptoValues) => {
+    try {
+        const bkngToken = new BkngToken(cryptoValues.passphrase, cryptoValues.iv);
+        return await bkngToken.decrypt(token);
+    } catch (err) {
+        console.error(chalk.red(err));
+    }
+}
+
+const getValuesFromConfig = async (config, phone) => {
+    let passphrase;
+    let iv;
+    if (phone) {
+        passphrase = argv.passphrase || await config.valueFor('booking.freeTaxiCellphonePassphrase');
+        iv = argv.iv || await config.valueFor('booking.freeTaxiCellphoneIV');
+    } else {
+        passphrase = argv.passphrase || await config.valueFor('booking.freeTaxiTokenPassphrase');
+        iv = argv.iv || await config.valueFor('booking.freeTaxiTokenIV');
+    }
+    return { passphrase, iv };
+}
+
+module.exports.decryptToken = decryptToken;
--- test/decrytUtil.spec.js
+++ test/decrytUtil.spec.js
@@ -1,5 +1,5 @@
 const decryptUtil = require('../src/decryptUtil');
-
+const decrypt = require('../src/v2/decrypt');
 const testPassphrase = 'I been feeling it since 1966, now';
 const testEncryptedToken = 'c11c51186e0ff651b5c43d14e8b34dd5203ad8ce470bc1c79a231976adea231f4dba7b940c332f68b8cd012c67a62746a126762704a9eeaff1319cf2b968f8cbcd89992d0ad36ac77fdb90e50ce3e2825a88330217ee855bec43553472119337a01772361229dfdaa977d53ef9f1ef49cc7c8611145a6f4c08b1753c995f213eedfc5faef08f3ecb51ddb05b3a959a5768f73caff5e2ac4ef22820ce2712e0435456cea8e3f0bc583a17e179e4f1a64fa295d8e0177513563136cbe38b76b086f9319b9d3161b51c3da3f2a02f902d096ce61a6f401472663b1290a173be9b117571d518d0957ca80d7bccec967357363950dac304ac7b0fb35e0b3c00a2b7c0bf068cbf1e63a450d33b17086398572b198115ba1cedf1520fa5f44248c4ba55a2e32c3ecba5295a12070fad04da35c71dbd76ab99750e7903d155b5ea6823e9b750b4129d26c63811781ac8d78288bd';
 const expectedDecryptedBody = {
@@ -23,6 +23,13 @@ const expectedDecryptedBody = {
         lastName: 'Team',
     },
 };
+const phoneTokenToDecrypt = 'izcv5O6lG2SG9xJgSojiwk9u5fMhgyeXoAvDMx1BgXE';
+
+const expectedCellphone = "+44 99999 60001";
+
+
+const passphrase = "this is the passphrase for cellphone encryption lol";
+const iv = "testingg";
 
 describe('Decryption Utility', () => {
     describe('When decrypting some data', () => {
@@ -67,49 +74,62 @@ describe('Decryption Utility', () => {
                 expect(result).toMatch(JSON.stringify(expectedDecryptedBody));
             });
         });
-    });
 
-    describe('When encrypting some data', () => {
-        describe('When an object to encrypt is not supplied', () => {
-            it('Should throw an error', async () => {
-                const expectedBodyError = new Error('Please supply some data to encrypt');
+        describe('When both a token and a passphrase are supplied and phone arg is pass', () => {
+            it('Should return an decrypted phone body', async () => {
                 let result;
+                const tokenToDecrypt = phoneTokenToDecrypt;
                 try {
-                    result = await decryptUtil.encrypt();
+                    result = await decrypt.decryptToken(tokenToDecrypt, { passphrase, iv });
                 } catch (error) {
                     result = error;
                 }
-
-                expect(result).toEqual(expectedBodyError);
+                expect(result.cellphone).toBe(expectedCellphone);
             });
         });
 
-        describe('When a passphrase to encrypt some data is not supplied', () => {
-            it('Should throw an error', async () => {
-                const expectedPassphraseError = new Error('Please supply a passphrase to encrypt token with');
-                let result;
-                try {
-                    result = await decryptUtil.encrypt(testPassphrase);
-                } catch (error) {
-                    result = error;
-                }
+        describe('When encrypting some data', () => {
+            describe('When an object to encrypt is not supplied', () => {
+                it('Should throw an error', async () => {
+                    const expectedBodyError = new Error('Please supply some data to encrypt');
+                    let result;
+                    try {
+                        result = await decryptUtil.encrypt();
+                    } catch (error) {
+                        result = error;
+                    }
 
-                expect(result).toEqual(expectedPassphraseError);
+                    expect(result).toEqual(expectedBodyError);
+                });
+            });
+
+            describe('When a passphrase to encrypt some data is not supplied', () => {
+                it('Should throw an error', async () => {
+                    const expectedPassphraseError = new Error('Please supply a passphrase to encrypt token with');
+                    let result;
+                    try {
+                        result = await decryptUtil.encrypt(testPassphrase);
+                    } catch (error) {
+                        result = error;
+                    }
+
+                    expect(result).toEqual(expectedPassphraseError);
+                });
+            });
+
+            describe('When both a token and a passphrase are supplied', () => {
+                it('Should return an encrypted body', async () => {
+                    let result;
+                    const bodyToEncrypt = JSON.stringify(expectedDecryptedBody);
+                    try {
+                        result = await decryptUtil.encrypt(bodyToEncrypt, testPassphrase);
+                    } catch (error) {
+                        result = error;
+                    }
+
+                    expect(result).toBe(testEncryptedToken);
+                });
             });
         });
-
-        describe('When both a token and a passphrase are supplied', () => {
-            it('Should return an encrypted body', async () => {
-                let result;
-                const bodyToEncrypt = JSON.stringify(expectedDecryptedBody);
-                try {
-                    result = await decryptUtil.encrypt(bodyToEncrypt, testPassphrase);
-                } catch (error) {
-                    result = error;
-                }
-
-                expect(result).toBe(testEncryptedToken);
-            });
-        });
-    });
+    })
 });
